639aaa0dd3d6aa4a0ae89cd4e7da7ff6fe064f87
Daniel Cherman <32578659+DanielCherman@users.noreply.github.com>
2020-05-20 16:38:27 +0200

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,37 +1,66 @@
 <template>
  <div class="row items-center justify-center">
-   <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert your name</div>
-      <q-input filled v-model="name" type="text" class="input-size q-mt-sm" label="Name" />
-   </div>
-   <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert your address</div>
-      <q-input filled v-model="address" type="text" class="input-size q-mt-sm" label="Address" />
-   </div>
-   <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert Phone Number</div>
-      <q-input filled v-model="phone" type="text" class="input-size q-mt-sm" mask="+370########" label="Phone" />
-   </div>
-   <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert Post Code</div>
-      <q-input filled v-model="post" type="text" class="input-size q-mt-sm" mask="#####" label="Post" />
-   </div>
-    <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert Email</div>
-      <q-input filled v-model="email" type="email" class="input-size q-mt-sm" label="Email" />
-   </div>
-    <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert Card number</div>
-      <q-input filled v-model="email" type="number" class="input-size q-mt-sm" label="cardNumber" />
-   </div>
-    <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert Expiery Date</div>
-      <q-input filled v-model="email" type="number" class="input-size q-mt-sm" label="expDate" />
-   </div>
-       <div class="data-name q-mt-xl q-ml-xl">
-     <div class="text">Insert Expiery CVV</div>
-      <q-input filled v-model="email" type="number" class="input-size q-mt-sm" label="cvv" />
-   </div>
+<q-card>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="user" label="User Info" />
+          <q-tab name="payment" label="Request Payment" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="user" class="row justify-center">
+            <div class="data-name q-mt-xl q-ml-xl">
+              <div class="text">Insert your name</div>
+                <q-input filled v-model="name" type="text" class="input-size q-mt-sm" label="Name" />
+            </div>
+            <div class="data-name q-mt-xl q-ml-xl">
+              <div class="text">Insert your address</div>
+                <q-input filled v-model="address" type="text" class="input-size q-mt-sm" label="Address" />
+            </div>
+            <div class="data-name q-mt-xl q-ml-xl">
+              <div class="text">Insert Phone Number</div>
+                <q-input filled v-model="phone" type="text" class="input-size q-mt-sm" mask="+370########" label="Phone" />
+            </div>
+            <div class="data-name q-mt-xl q-ml-xl">
+              <div class="text">Insert Post Code</div>
+                <q-input filled v-model="post" type="text" class="input-size q-mt-sm" mask="#####" label="Post" />
+            </div>
+              <div class="data-name q-mt-xl q-ml-xl">
+              <div class="text">Insert Email</div>
+                <q-input filled v-model="email" type="email" class="input-size q-mt-sm" label="Email" />
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="payment" class="row justify-center">
+            <div class="data-name q-mt-xl q-ml-xl">
+              <div class="text">Insert your name</div>
+                <q-input filled v-model="name" type="text" class="input-size q-mt-sm" label="Name" />
+            </div>
+            <div class="data-name q-mt-xl q-ml-xl">
+                <div class="text">Insert Card number</div>
+                  <q-input filled v-model="cardNumber" type="number" class="input-size q-mt-sm" label="Card Number" />
+              </div>
+                <div class="data-name q-mt-xl q-ml-xl">
+                <div class="text">Insert Expiry Date</div>
+                  <q-input filled v-model="expDate" type="number" class="input-size q-mt-sm" label="Expiry Date" />
+              </div>
+                  <div class="data-name q-mt-xl q-ml-xl">
+                <div class="text">Insert CVV</div>
+                  <q-input filled v-model="cvv" type="number" class="input-size q-mt-sm" label="CVV" />
+                 </div>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+ 
 
 
 
@@ -50,7 +79,8 @@ export default {
       email: '',
       cardNumber: '',
       expDate: '',
-      cvv: ''
+      cvv: '',
+      tab: 'user',
     }
   }
 }
@@ -61,5 +91,8 @@ export default {
 }
 .text{
   font-size: 25px;
+}
+.q-card{
+  width: 100%;
 }
 </style>
